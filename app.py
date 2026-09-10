d87eff22926d6edd248faf110343382b2ef79c4f
Izam8888 <mashizam2@gmail.com>
2026-09-10 19:05:13 +0700

    Initial EduPulse Streamlit project

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="EduPulse",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("EduPulse")
+st.write("Student Performance Analytics & Early Warning System")
---
+++ predict.py
@@ -0,0 +1,48 @@
+
+import joblib
+import os
+import pandas as pd
+
+MODEL_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "edupulse_model_final.joblib"
+)
+
+model_package = joblib.load(MODEL_PATH)
+
+model = model_package["model"]
+thresholds = model_package["risk_thresholds"]
+
+
+def predict_student(
+    attendance,
+    quiz_1_score,
+    quiz_2_score,
+    assignment_score,
+    daily_study_hours
+):
+    quiz_1_score_pct = (quiz_1_score / 40) * 100
+    quiz_2_score_pct = (quiz_2_score / 40) * 100
+    assignment_pct = (assignment_score / 10) * 100
+
+    features = pd.DataFrame([{
+        "attendance": attendance,
+        "quiz_1_score_pct": quiz_1_score_pct,
+        "quiz_2_score_pct": quiz_2_score_pct,
+        "assignment_pct": assignment_pct,
+        "daily_study_hours": daily_study_hours
+    }])
+
+    predicted_score = model.predict(features)[0]
+
+    if predicted_score < thresholds["high_risk"]:
+        risk = "High Risk"
+    elif predicted_score < thresholds["medium_risk"]:
+        risk = "Medium Risk"
+    else:
+        risk = "Low Risk"
+
+    return {
+        "predicted_score": round(float(predicted_score), 2),
+        "risk": risk
+    }
